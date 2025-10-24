c75ace1a9af29824b03d3e81ce90021d0f1bd0c2
Jabir Minjibir <jabir.minjibir@gmail.com>
2025-10-24 12:30:10 -0400

    Implements delete endpoint.

--- src/main/java/com/minjibir/resource/TaskResource.java
+++ src/main/java/com/minjibir/resource/TaskResource.java
@@ -1,17 +1,18 @@
 package com.minjibir.resource;
 
 import com.minjibir.dto.TaskResponse;
+import com.minjibir.model.Task;
 import com.minjibir.repository.TaskRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Path("/api/tasks")
 @ApplicationScoped
@@ -36,5 +37,19 @@ public class TaskResource {
             task.createdAt,
             task.updatedAt
          )).toList();
+   }
+
+   @DELETE
+   @Path("/{id}")
+   @Transactional
+   public Response deleteTask(UUID id) {
+      Optional<Task> optTask = taskRepository.findByIdOptional(id);
+
+      if (optTask.isPresent()) {
+         taskRepository.deleteById(id);
+         return Response.noContent().build();
+      }
+
+      return Response.status(Response.Status.NOT_FOUND).build();
    }
 }
--- src/test/java/com/minjibir/resource/TaskResourceTest.java
+++ src/test/java/com/minjibir/resource/TaskResourceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -71,5 +72,33 @@ public class TaskResourceTest {
          .body("[1].title", is(tasks.get(1).title))
          .body("[1].description", is(tasks.get(1).description))
          .body("[1].status", is(tasks.get(1).status.toString()));
+   }
+
+   @Test
+   void deleteTask_shouldReturnNotFoundIfTheTaskWithTheSuppliedIdDoesNotExists() {
+      var nonExistingId = UUID.randomUUID();
+
+      given()
+         .when()
+         .delete("/api/tasks/{id}", nonExistingId)
+         .then()
+         .statusCode(404)
+         .body(is(""));
+   }
+
+   @Test
+   void deleteTask_shouldReturnNoContentIfTheTaskWithTheSuppliedIdExists() throws Exception {
+
+      userTransaction.begin();
+      taskRepository.persist(tasks.getFirst());
+      taskRepository.flush();
+      userTransaction.commit();
+
+      given()
+         .when()
+         .delete("/api/tasks/" + tasks.getFirst().id.toString())
+         .then()
+         .statusCode(204)
+         .body(is(""));
    }
 }
